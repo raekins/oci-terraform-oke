b9d821e4139eb008f0fb844d057a49794cd1b390
Ron Ekins <ron.ekins@gmail.com>
2021-10-08 15:15:35 +0100

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    oci = {
+      source = "hashicorp/oci"
+      version = "4.37.0"
+    }
+  }
+}
+
+provider "oci" {
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  private_key_path = var.private_key_path
+  fingerprint      = var.fingerprint
+  region           = var.region
+}
